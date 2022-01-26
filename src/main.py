89897a0c49995a20e8355fd04668bd5ef49844a1
BlackChesire <asafbs177@gmail.com>
2022-01-26 13:15:02 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+import sqlite3
+import sys
+import src.db_util as db_utils
+import src.conf as conf
+
+if __name__ == '__main__':
+    connection = None
+    try:
+        connection = db_utils.create_db()
+        db_utils.load_raw_data(connection)
+        db_utils.select_all_by_table(connection, conf.PHONE_TABLE)
+        db_utils.select_all_by_table(connection, conf.SALE_TABLE)
+    except sqlite3.Error as e:
+        print(f"Error {e.args[0]}")
+        sys.exit(1)
+    finally:
+        if connection:
+            connection.close()
