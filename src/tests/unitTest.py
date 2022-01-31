cec94d9d8263694f7cd1b8ea568f7d5d7541b876
BlackChesire <asafbs177@gmail.com>
2022-01-31 12:11:51 +0200

    .

--- src/tests/unitTest.py
+++ src/tests/unitTest.py
@@ -13,6 +13,7 @@ import yaml
 
 @pytest.fixture
 def db():
+    """configure test DB """
     if os.path.isfile(conf.TEST_DB):
         print("Database exist, skip create")
         return sqlite3.connect(f'file:{conf.TEST_DB}?mode=rw', uri=True)
@@ -31,41 +32,40 @@ def test_add_new_phone(db):
     db_utils.add_new_phone(db, xiaomi)
     db_utils.add_new_phone(db, mini)
     mi10 = db_utils.get_phone_by_model(db, "mi10")
-    print(mi10[5])
     iphone_x = db_utils.get_phone_by_model(db, "x")
     mini_12 = db_utils.get_phone_by_model(db, "12 mini")
 
-#
-# def test_update_phone_quantity(db):
-#     db_utils.update_phone_quantity(db, "mi10", 1)
-#     db_utils.update_phone_quantity(db, "x", 3)
-#     db_utils.update_phone_quantity(db, "12 mini", 0)
-#     mi10 = db_utils.get_phone_by_model(db, "mi10")
-#     iphone_x = db_utils.get_phone_by_model(db, "x")
-#     mini_12 = db_utils.get_phone_by_model(db, "12 mini")
-#
-#
-# def test_add_new_sale(db):
-#     xiaomi_sale = Sale("xiaomi", "mi10", "22 - 1 - 21", 1, 2400, 0.16)
-#     iphone_sale = Sale("iphone", "x", "20 - 3 - 22", 1, 2000, 0)
-#     db_utils.get_sales_by_date(db, "22 - 1 - 21")
-#     pass
-#
-#
-# def test_sales_by_date(db):
-#     pass
-#
-#
-# def test_get_phone_stock_report(db):
-#     db_utils.get_phone_stock_report(db)
-#     pass
-#
-#
-# def test_sales_report_by_date():
-#     pass
-#
-#
-# def test_get_price_by_model():
-#     iphone_price = db_utils.get_price_by_model(db, "x", "iphone")
-#     samsung_price = db_utils.get_price_by_model(db, "s10", "samsung")
-#     pass
+
+def test_update_phone_quantity(db):
+    db_utils.update_phone_quantity(db, "mi10", 1)
+    db_utils.update_phone_quantity(db, "x", 3)
+    db_utils.update_phone_quantity(db, "12 mini", 0)
+    mi10 = db_utils.get_phone_by_model(db, "mi10")
+    iphone_x = db_utils.get_phone_by_model(db, "x")
+    mini_12 = db_utils.get_phone_by_model(db, "12 mini")
+
+
+def test_add_new_sale(db):
+    xiaomi_sale = Sale("xiaomi", "mi10", "22 - 1 - 21", 1, 2400, 0.16)
+    iphone_sale = Sale("iphone", "x", "20 - 3 - 22", 1, 2000, 0)
+    db_utils.get_sales_by_date(db, "22 - 1 - 21")
+    pass
+
+
+def test_sales_by_date(db):
+    pass
+
+
+def test_get_phone_stock_report(db):
+    db_utils.get_phone_stock_report(db)
+    pass
+
+
+def test_sales_report_by_date():
+    pass
+
+
+def test_get_price_by_model():
+    iphone_price = db_utils.get_price_by_model(db, "x", "iphone")
+    samsung_price = db_utils.get_price_by_model(db, "s10", "samsung")
+    pass
